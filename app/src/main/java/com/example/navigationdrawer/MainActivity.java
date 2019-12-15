e0f2bd99c3662ffcc6bd8bc4035d1960a93cd734
kaurharpreet0003 <preetjandu0003@gmail.com>
2019-12-14 21:16:34 -0500

    navigation_fragments

--- app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
@@ -28,26 +29,46 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout = findViewById(R.id.drawer);
         toolbar = findViewById(R.id.toolBar);
         navigationView = findViewById(R.id.navigationView);
-
+        navigationView.setNavigationItemSelectedListener(this);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawerOpen,R.string.drawerClose);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+//        if (savedInstanceState == null) {
+//        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new contact()).commit();
+//        navigationView.setCheckedItem(R.id.contact);
+//        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+           drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        if (menuItem.getItemId() == R.id.contact) {
+        if (menuItem.getItemId() == R.id.home){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new home()).commit();
+        }else if (menuItem.getItemId() == R.id.contact) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new contact()).commit();
             Toast.makeText(MainActivity.this, "contact selected", Toast.LENGTH_SHORT).show();
         } else if (menuItem.getItemId() == R.id.about) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new about()).commit();
             Toast.makeText(MainActivity.this, "about selected", Toast.LENGTH_SHORT).show();
         } else if (menuItem.getItemId() == R.id.feedback){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new feedback()).commit();
             Toast.makeText(MainActivity.this, "feedback selected", Toast.LENGTH_SHORT).show();
         }else if (menuItem.getItemId() == R.id.logout) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new logout()).commit();
             Toast.makeText(MainActivity.this, "logout selected", Toast.LENGTH_SHORT).show();
         }
+        drawerLayout.closeDrawer(GravityCompat.START);
         return false;
     }
 }
